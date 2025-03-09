efff27480c456a6beed8c5957ce506022493de15
Vencislav Atanasov <git@6bez10.info>
2025-03-09 11:55:33 +0200

    Implement parsing of bill validator identification (level 1 and level 2+) packet

--- src/mdb/billValidator.js
+++ src/mdb/billValidator.js
@@ -1,4 +1,4 @@
-import { hexToBuffer } from '../util/index.js';
+import { hexToBuffer, toInt } from '../util/index.js';
 import { readByteArray, splitToBits } from './util.js';
 
 export function parseSetupResponse(hex) {
@@ -8,7 +8,7 @@ export function parseSetupResponse(hex) {
         throw new Error('Invalid setup response length, expected 27, got ' + bytes.length);
     }
 
-    const countryCurrencyCode = parseInt(hex.substring(2, 6), 10); // BCD encoding
+    const countryCurrencyCode = toInt(hex.substring(2, 6)); // BCD encoding
 
     return {
         billValidatorFeatureLevel: bytes.readUInt8(),
@@ -28,6 +28,11 @@ export function parseSetupResponse(hex) {
 
 export function parseStackerResponse(hex) {
     const bytes = hexToBuffer(hex);
+
+    if (bytes.length !== 2) {
+        throw new Error('Invalid stacker response length, expected 2, got ' + bytes.length);
+    }
+
     const response = bytes.readUInt16BE();
 
     return {
@@ -38,3 +43,40 @@ export function parseStackerResponse(hex) {
 
 // console.log(parseStackerResponse('0014'));
 // console.log(parseStackerResponse('8164'));
+
+export function parseLevel1IdentificationWithoutOptionBits(hex) {
+    const bytes = hexToBuffer(hex);
+
+    if (bytes.length !== 29) {
+        throw new Error('Invalid level 1 identification length, expected 29, got ' + bytes.length);
+    }
+
+    return {
+        manufacturerCode: bytes.toString('ascii', 0, 3),
+        serialNumber: bytes.toString('ascii', 3, 15),
+        modelNumber: bytes.toString('ascii', 15, 27),
+        softwareVersion: toInt(hex.substring(27 * 2)),
+    };
+}
+
+// console.log(parseLevel1IdentificationWithoutOptionBits('49544c3030303030363138353136314e56313120333834203030300425'));
+
+export function parseLevel2PlusIdentificationWithOptionBits(hex) {
+    const bytes = hexToBuffer(hex);
+
+    if (bytes.length !== 33) {
+        throw new Error('Invalid level 2+ identification length, expected 33, got ' + bytes.length);
+    }
+
+    const optionalFeatures = bytes.readUInt32BE(29);
+
+    return {
+        ...parseLevel1IdentificationWithoutOptionBits(hex.substring(0, 29 * 2)),
+        optionalFeatures: {
+            ftl: (optionalFeatures & 0x1) === 0x1,
+            billRecycling: (optionalFeatures & 0x2) === 0x2,
+        },
+    };
+}
+
+// console.log(parseLevel2PlusIdentificationWithOptionBits('49544c3030303030363138353136314e5631312033383420303030042500000002'));
