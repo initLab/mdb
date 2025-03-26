de190169eb11213453d13b84f64bceb2194e70ac
Vencislav Atanasov <git@6bez10.info>
2025-03-26 04:00:54 +0200

    Add parseAnswer function

--- src/qibixx/util.js
+++ src/qibixx/util.js
@@ -4,3 +4,19 @@ export function parseVersion(str) {
     const [ major, minor, patch, build] = str.split('.').map(toInt);
     return { major, minor, patch, build };
 }
+
+export function parseAnswer(str, allowData = false) {
+    if (str === 'ACK') {
+        return true;
+    }
+
+    if (str === 'NACK') {
+        return false;
+    }
+
+    if (!allowData) {
+        throw new Error(`Unexpected answer: ${str}`);
+    }
+
+    return str;
+}
