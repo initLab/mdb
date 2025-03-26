e9010eee998b96b227e6881059bc8a1f9f548bdc
Vencislav Atanasov <git@6bez10.info>
2025-03-26 04:00:28 +0200

    Use Node timers for sleep

--- src/util/index.js
+++ src/util/index.js
@@ -1,5 +1,5 @@
-import { promisify } from 'node:util';
+import { setTimeout } from 'node:timers/promises';
 
-export const sleep = promisify(setTimeout);
+export const sleep = async delay => await setTimeout(delay);
 export const toInt = str => parseInt(str, 10);
 export const hexToBuffer = hex => Buffer.from(hex, 'hex');
